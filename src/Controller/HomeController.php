eaf6ec37d2f38d4a1a552cb0f265e433f5303ab7
badi3a <badiaabouhdid@gmail.com>
2022-02-08 11:42:14 +0100

    WORKSHOP ENTITY + BD

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,5 +25,27 @@ class HomeController extends AbstractController
     public function about(): Response
     {
         return $this->render('home/about.html.twig');
+    }
+
+    /**
+     * @Route("/add", name="user_add", methods={"GET", "POST"})
+     */
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('home/add.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
     }
 }
